57e5c721ee95bf07e978c8e8ef12a45689e9d56e
ignaciogatti <nacho.gatti@gmail.com>
2019-08-08 18:24:47 -0300

    navigation bar responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,29 +8,36 @@ class Header extends React.Component{
 
     render(){
         return(
-            <header id="home">
-                <nav id="nav-wrap">
-                <ul id="nav">
-                    <li className="current">
-                        <Link to="/">Artwork Curator</Link>
-                    </li>
+        <header id="home">
+            
+            <nav id="nav-wrap">
+
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+            <ul id="nav" className="nav">
+                <li className="current">
+                    <Link className="header-link" to="/">Artwork Curator</Link>
+                </li>
+                <li>
+                    <Link className="header-link" to="/aboutme">About me</Link>
+                </li>
+
+                {this.props.isSignedIn && (
                     <li>
-                        <Link to="/aboutme">About me</Link>
-                    </li>
-                    {this.props.isSignedIn && (
-                        <li>
-                        <Link to="/aboutme">Tests</Link>
+                        <Link className="header-link" to="/aboutme">Tests</Link>
                     </li>
                     )}
                     
-                    <li className="auth">
-                        <GoogleAuth />
-                    </li>
-                </ul>
-        
-                </nav>
-        
-            </header>
+                <li className="auth">
+                    <GoogleAuth />
+                </li>
+
+            </ul>
+    
+            </nav>
+    
+        </header>
         );
     }
 }
