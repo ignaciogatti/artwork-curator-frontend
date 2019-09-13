28fc605dd5e023a8c6895294372da96d94bf0f39
ignaciogatti <nacho.gatti@gmail.com>
2019-09-13 18:20:05 -0300

    Changes in description paragraph

--- src/apis/artworkRetrieval.js
+++ src/apis/artworkRetrieval.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://art-retrieval-api-234614.appspot.com/'
+    //baseURL: 'https://devstandw-dot-art-retrieval-api-234614.appspot.com/'
     //baseURL: 'http://localhost:8000/'
 });
--- src/components/AboutDCurator.js
+++ src/components/AboutDCurator.js
@@ -10,11 +10,11 @@ class AboutDCurator extends React.Component{
                         <h2>About D-Curator</h2>
                         <p>
                             D-Curator is a AI developed with Deep Learning techniques. Basically, these techniques allow the machine to analize
-                            the images taking into account only the pixel representation. In other words, D-Curator has no idea about who painted each artwork,
-                            or the history behind each one, or what it means when someone says "this is an impressionist painting". 
+                            the images taking into account only pixel representation. In other words, D-Curator has no idea about who painted each artwork,
+                            or the history behind it, or what it means when someone says "this is an impressionist painting". 
                             <br/>
-                            It only looks at image and takes it own conclusions. Despite of the fact that it doesn't know anything about Art History, 
-                            it is able to find interesting relationships between artworks. Indeed, it seems that D-Curator can find artworks that have 
+                            It only looks at the image and takes its own conclusions. Despite of the fact that it doesn't know anything about Art History, 
+                            it is able to find interesting connections between artworks. Indeed, it seems that D-Curator can find artworks that have 
                             a similar composition (color, structure, strength lines).
                         </p>
                     </div>
@@ -23,15 +23,15 @@ class AboutDCurator extends React.Component{
                     <div className="six columns main-col">
                         <h2>Experiment</h2>
                         <p>
-                            The objective of the experiment is to give a feedback to D-Curator. In order to improve its skills as artwork analyzer, it is 
-                            necessary to tell to D-Curator which choices were right and which ones not.
+                            The objective of the experiment is to provide feedback to D-Curator. In order to improve its skills as artwork analyzer, it is 
+                            necessary to tell which choices were right and which ones weren't.
                             <br/>
-                            In this experiment you will have an reference artwork and a set of artworks that were selected by D-Curator. Your task consists on 
-                            analyze if each artwork of the set is related to the reference artwork. Remember that D-Curator only takes into account the 
+                            In this experiment you will have a reference artwork and a set of artworks that were selected by D-Curator. Your task consists on 
+                            analyzing if each artwork of the set is related to the reference artwork. Remember that D-Curator only takes into account the 
                             compositon (color, structure, strength lines). Don't expect to find relationships like "same artist" or "same genre", because 
                             D-Curator doesn't know anything about Art History. 
                             <br/>
-                            Also, you have to know that D-Curator only saw artworks from the following periods: Baroque, Romanticism, Realism, 
+                            Also, you have to know that D-Curator only considered artworks from the following periods: Baroque, Romanticism, Realism, 
                             Impressionism, Expressionism, Surrealism, Symbolism and Art-Nouveau. Therefore, all the artworks in the set belong to those periods.
 
                         </p>
