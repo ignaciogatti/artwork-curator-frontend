30b03443039e84be577cf43a467e9eaa9c1dea4d
ignaciogatti <nacho.gatti@gmail.com>
2020-03-18 17:03:01 -0300

    Content-based experiment

---
+++ src/data/experimentData/contentBasedExperiment/dataExperimentContentBased.js
@@ -0,0 +1,10 @@
+import experimentData2 from './experimentData2';
+import experimentData3 from './experimentData3';
+import experimentData4 from './experimentData4';
+import experimentData5 from './experimentData5';
+import experimentData from './experimentData';
+
+export const data_experiment = {
+    data : [experimentData, experimentData2, experimentData3, experimentData4, experimentData5],
+    experimentType : 'contentBased'
+}
